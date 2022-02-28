fea20edd6869ad91f2bab81ba22cc008a34e27e7
Hong Gia Lap <laphg97@gmail.com>
2022-02-28 08:06:57 +0700

    Implementing: - Scene (WIP)

--- Engine/Scene.h
+++ Engine/Scene.h
@@ -3,7 +3,9 @@
 #define __SCENE_H__
 
 #pragma region INCLUDE
-
+#include <vector>
+#include <map>
+#include <string>
 #pragma endregion
 
 namespace Engine
@@ -16,13 +18,40 @@ namespace Engine
 
 	struct Scene
 	{
+		std::string _source;
+		std::vector<unsigned int> _gameObjects;
+		bool _play = false;
+		bool _load = false;
 
+		Scene(std::string source)
+		{
+			_source = source;
+		}
+
+		void AddGameObject(unsigned int id);
+		void RemoveGameObject(unsigned int id);
 	};
 	typedef Scene* pScene;
 
 	class Scenes
 	{
+	private:
+		pGame _game;
+		std::map<unsigned int, pScene> _scenes;
+		bool _loading = false;
 
+	public:
+		Scenes(pGame game) { _game = game; }
+		void AddScene(unsigned int id, std::string source);
+
+		void Loading();
+		void LoadScene(pScene scene);
+		void UnloadScene(pScene scene);
+
+		void PlayScene(unsigned int id);
+		void StopScene(unsigned int id);
+
+		void Shutdown();
 	};
 	typedef Scenes* pScenes;
 
