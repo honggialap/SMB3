38aae7d2c52f2ebaab8b3006ddccf85ca047432a
Hong Gia Lap <laphg97@gmail.com>
2022-02-28 07:17:45 +0700

    Implementing: - Player

--- Engine/Player.h
+++ Engine/Player.h
@@ -13,11 +13,27 @@ namespace Engine
 	class Player : public KeyHandler
 	{
 	private:
+		std::map<int, bool> _currentButtonState;
+		std::map<int, bool> _previousButtonState;
+
+		float _cameraX = 0;
+		float _cameraY = 0;
+		float _cameraBuffer = 0;
 
 	public:
 		virtual void KeyState();
 		virtual void OnKeyUp(int keyCode);
 		virtual void OnKeyDown(int keyCode);
+
+		void BindKey(int keyCode);
+		bool IsKeyDown(int keyCode);
+		bool IsKeyPressed(int keyCode);
+		bool IsKeyReleased(int keyCode);
+
+		float GetCameraBuffer() { return _cameraBuffer; }
+		void GetCamera(float& x, float& y) { x = _cameraX; y = _cameraY; }
+		void MoveCameraTo(float x, float y) { _cameraX = x; _cameraY = y; }
+		void MoveCameraBy(float x, float y) { _cameraX += x; _cameraY += y; }
 	};
 	typedef Player* pPlayer;
 
