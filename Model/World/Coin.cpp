6d42a3ba1c8e4f36d6430643acbf58e5cb4129a0
Hong Gia Lap <laphg97@gmail.com>
2022-04-17 13:29:29 +0700

    mirgrating coin.

--- Model/World/Coin.cpp
+++ Model/World/Coin.cpp
@@ -4,19 +4,147 @@
 #pragma endregion
 
 void CCoin::Load() {
+	CGameObject::Load();
+
+	/* Read file */
+	pugi::xml_document prefabDoc;
+	prefabDoc.load_file(_source.c_str());
+
+	/* Body */
+	auto bodyNode = prefabDoc.child("Prefab").child("Body");
+	_renderBody = bodyNode.attribute("render").as_bool();
+	BODY_WIDTH = bodyNode.attribute("width").as_float();
+	BODY_HEIGHT = bodyNode.attribute("height").as_float();
+	BODY_OFFSETX = bodyNode.attribute("offsetX").as_float();
+	BODY_OFFSETY = bodyNode.attribute("offsetY").as_float();
+
+	/* Spin */
+	auto spinNode = prefabDoc.child("Prefab").child("Spin");
+	GRAVITY = spinNode.attribute("GRAVITY").as_float();
+	BOUNCE_FORCE = spinNode.attribute("BOUNCE_FORCE").as_float();
+	DECAY_INTERVAL = spinNode.attribute("DECAY_INTERVAL").as_float();
+	_spin = spinNode.attribute("spin").as_bool();
 }
 
 void CCoin::Start() {
+	CGameObject::Start();
 }
 
 void CCoin::Update(float elapsedMs) {
+	if (!_start) {
+		Start();
+	}
+
+	HandleAction(elapsedMs);
 }
 
 void CCoin::Render() {
+	if (_renderBody) {
+		_sprites[SPR_BBOX]->Render(_x, _y);
+	}
+
+	switch (_action) {
+	case CCoin::EAction::IDLE: {
+		if (_spin) {
+			_animations[ANI_COIN_SPIN_SLOW]->Render(_x, _y);
+		}
+		else {
+			_sprites[SPR_COIN1]->Render(_x, _y);
+		}
+	} break;
+
+	case CCoin::EAction::CONSUMED: {
+		_animations[ANI_COIN_SPIN_FAST]->Render(_x, _y);
+	} break;
+
+	}
 }
 
+#pragma region STATE MACHINE
+
+void CCoin::HandleAction(float elapsedMs) {
+	switch (_action) {
+	case CCoin::EAction::IDLE: {
+		Idle(elapsedMs);
+	} break;
+
+	case CCoin::EAction::CONSUMED: {
+		Consumed(elapsedMs);
+	} break;
+
+	}
+}
+
+void CCoin::Idle(float elapsedMs) {
+	switch (_actionStage) {
+	case CCoin::EActionStage::ENTRY: {
+		_animations[ANI_COIN_SPIN_SLOW]->Play(true);
+		_actionStage = EActionStage::PROGRESS;
+	} break;
+
+	case CCoin::EActionStage::PROGRESS: {
+		_animations[ANI_COIN_SPIN_SLOW]->Update(elapsedMs);
+	} break;
+
+	case CCoin::EActionStage::EXIT: {
+		_animations[ANI_COIN_SPIN_SLOW]->Stop();
+		NextAction();
+	} break;
+
+	}
+}
+
+void CCoin::Consumed(float elapsedMs) {
+	switch (_actionStage) {
+	case CCoin::EActionStage::ENTRY: {
+		_animations[ANI_COIN_SPIN_FAST]->Play(true);
+		_decayInterval = DECAY_INTERVAL;
+		_vy = BOUNCE_FORCE;
+		_actionStage = EActionStage::PROGRESS;
+	} break;
+
+	case CCoin::EActionStage::PROGRESS: {
+		_animations[ANI_COIN_SPIN_FAST]->Update(elapsedMs);
+
+		if (_decayInterval > 0) {
+			_decayInterval -= elapsedMs;
+			_vy -= GRAVITY * elapsedMs;
+		}
+		else {
+			_animations[ANI_COIN_SPIN_FAST]->Stop();
+			Destroy();
+		}
+
+		_x += _vx * elapsedMs;
+		_y += _vy * elapsedMs;
+	} break;
+
+	case CCoin::EActionStage::EXIT: {
+		NextAction();
+	} break;
+
+	}
+}
+
+#pragma endregion
+
+#pragma region COLLISION
+
 int CCoin::IsCollidable() {
-	return 0;
+	switch (_action) {
+	case CCoin::EAction::IDLE: {
+		return 1;
+	} break;
+
+	case CCoin::EAction::CONSUMED: {
+		return 0;
+	} break;
+
+	default: {
+		return 0;
+	} break;
+
+	}
 }
 
 int CCoin::IsBlocking() {
@@ -24,4 +152,24 @@ int CCoin::IsBlocking() {
 }
 
 void CCoin::GetBoundingBox(float& left, float& top, float& right, float& bottom) {
+	switch (_action) {
+	case CCoin::EAction::IDLE: {
+		left = _x + BODY_OFFSETX - (BODY_WIDTH / 2);
+		right = _x + BODY_OFFSETX + (BODY_WIDTH / 2);
+		top = _y + BODY_OFFSETY + (BODY_HEIGHT / 2);
+		bottom = _y + BODY_OFFSETY - (BODY_HEIGHT / 2);
+	} break;
+
+	case CCoin::EAction::CONSUMED: {
+	} break;
+	
+	}
 }
+
+void CCoin::OnNoCollision(float elapsedMs) {
+}
+
+void CCoin::OnCollisionWith(pCollision collision) {
+}
+
+#pragma endregion
--- Model/World/Coin.h
+++ Model/World/Coin.h
@@ -7,15 +7,29 @@
 #pragma endregion
 
 #pragma region COMPONENT ID
+/* SPRITE ID */
+#define SPR_BBOX				999901
+
+#define SPR_COIN1				100101
+#define SPR_COIN2				100102
+#define SPR_COIN3				100103
+#define SPR_COIN4				100104
+#define SPR_COIN5				100105
+#define SPR_COIN6				100106
+
+/* ANIMATION ID */
+#define ANI_COIN_SPIN_SLOW		1001
+#define ANI_COIN_SPIN_FAST		1002
+
 #pragma endregion
 
 class CCoin : public CGameObject {
 public:
 	CCoin(
 		pGame game, pScene scene,
-		unsigned int id, std::string name, std::string source,
+		unsigned int ID, std::string name, std::string source,
 		float x, float y, int gx, int gy, unsigned int layer
-	) : CGameObject(game, scene, id, name, source, x, y, gx, gy, layer) {};
+	) : CGameObject(game, scene, ID, name, source, x, y, gx, gy, layer) {};
 
 public:
 	virtual void Load();
@@ -23,9 +37,69 @@ public:
 	virtual void Update(float elapsedMs);
 	virtual void Render();
 
+#pragma region LOGIC
+
+	/* Body */
+	bool _renderBody = false;
+	float BODY_WIDTH = 0;
+	float BODY_HEIGHT = 0;
+	float BODY_OFFSETX = 0;
+	float BODY_OFFSETY = 0;
+
+	/* Spin */
+	float GRAVITY = 0;
+	float BOUNCE_FORCE = 0;
+	float DECAY_INTERVAL = 0;
+	float _decayInterval = 0;
+	bool _spin = false;
+
+#pragma endregion
+
+#pragma region STATE MACHINE
+
+	enum class EAction {
+		IDLE,
+		CONSUMED
+	};
+	EAction _action = EAction::IDLE;
+	EAction _nextAction = EAction::IDLE;
+
+	enum class EActionStage	{
+		ENTRY,
+		PROGRESS,
+		EXIT
+	};
+	EActionStage _actionStage = EActionStage::ENTRY;
+
+	void SetAction(EAction action) {
+		_action = action;
+		_actionStage = EActionStage::ENTRY;
+	}
+	void SetNextAction(EAction action) {
+		_nextAction = action;
+		_actionStage = EActionStage::EXIT;
+	}
+	void NextAction() {
+		_action = _nextAction;
+		_actionStage = EActionStage::ENTRY;
+	}
+
+	void HandleAction(float elapsedMs);
+	void Idle(float elapsedMs);
+	void Consumed(float elapsedMs);
+
+#pragma endregion
+
+#pragma region COLLISION
+
 	virtual int IsCollidable();
 	virtual int IsBlocking();
 	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+
+	void OnNoCollision(float elapsedMs);
+	void OnCollisionWith(pCollision collision);
+
+#pragma endregion
 };
 typedef CCoin* pCoin;
 
